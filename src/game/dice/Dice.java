dd7011564c0ccbaae90077ad119c8128aecc5e8c
Martin Slavov <martinslavov710@gmail.com>
2025-06-27 13:42:10 +0300

    Feature: Implement Dice class Adds the Dice class to the project. It is used for all dice rolls in the game.
    The class holds these details:
    - Methods to simulate rolling a 2-sided, 10-sided, and 100-sided dice
    - Javadoc have been added for clarity and documentation.

---
+++ src/game/dice/Dice.java
@@ -0,0 +1,49 @@
+package game.dice;
+
+import java.util.Random;
+
+/**
+ * The Dice class simulates rolling a dice with a variable number of sides.
+ * It provides methods to generate random number for different game mechanics.
+ */
+public class Dice {
+    private final Random random;
+
+    /**
+     * Constructs a new Dice object.
+     * Initializes the random number generator.
+     */
+    public Dice() {
+        this.random = new Random();
+    }
+
+
+    /**
+     * Rolls a 2-sided dice for player movement.
+     * The result is a random integer between 1 and 2, inclusive.
+     * @return A random integer (1 or 2).
+     */
+    public int rollTwoSidedDice() {
+        return random.nextInt(1,3);
+    }
+
+    /**
+     * Rolls a 10-sided dice.
+     * This is used to determine if a Chance card is positive or negative, or for trap-related logic.
+     * The result is a random integer between 1 and 10, inclusive.
+     * @return A random integer (1-10).
+     */
+    public int rollTenSidedDice() {
+        return random.nextInt(1,11);
+    }
+
+    /**
+     * Rolls a 100-sided dice.
+     * This is used to select a specific Chance card from an interval.
+     * The result is a random integer between 1 and 100, inclusive.
+     * @return A random integer (1-100).
+     */
+    public int rollHundredSidedDice() {
+        return random.nextInt(1,101);
+    }
+}
