67382105205803e7cb1625f373665cc55daf603d
Martin Slavov <martinslavov710@gmail.com>
2025-06-27 16:24:21 +0300

    Feature: Add SquareType enum It defines all the types of squares on the game board

---
+++ src/game/board/squares/SquareType.java
@@ -0,0 +1,13 @@
+package game.board.squares;
+
+/**
+ * An enum representing the different types of squares on the game board.
+ */
+public enum SquareType {
+    START,
+    INVEST,
+    CHANCE,
+    TRAP,
+    STEAL,
+    PARTY_HARD
+}
