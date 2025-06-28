a682974ee2c7c3006cb676fe8f2207e821b9b7e7
Martin Slavov <martinslavov710@gmail.com>
2025-06-28 12:27:53 +0300

    Feature: Implement StartSquare class Adds StartSquare class to the project.
    It represents the starting point on the board. When a player lands on this square.
    The `performAction` method adds a bonus amount to their balance.

---
+++ src/game/board/squares/StartSquare.java
@@ -0,0 +1,33 @@
+package game.board.squares;
+
+import game.players.Player;
+
+public class StartSquare extends Square {
+    // TODO: For better maintainability, this constant can be moved to a dedicated constants class.
+    private static final double BONUS_AMOUNT = 200.0;
+
+    /**
+     * Constructs the StartSquare.
+     * It is always named "Start" and has the START type.
+     *
+     * @param position The position of the square on the board (always 0).
+     */
+
+    public StartSquare(int position) {
+        super("Start", SquareType.START, position);
+    }
+
+    /**
+     * Defines the action to be performed when a player lands on this square.
+     * The player receives a bonus amount of money.
+     *
+     * @param player The player who lands on the square.
+     */
+    @Override
+    public void performAction(Player player) {
+        player.addMoney(BONUS_AMOUNT);
+        System.out.println(player.getName() + " landed on " + getName() +
+                " and received " + BONUS_AMOUNT + " money. " +
+                "New balance: " + player.getBalance());
+    }
+}
