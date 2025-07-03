ec3eb34a88f4df94d3c308a40069f0485991c7af
Martin Slavov <mstefslav@gmail.com>
2025-07-03 17:51:01 +0300

    Feature: Add main entry point for game launcher
    This commit introduces the `Main` class, which serves as the primary entry point
    for the "Evil Geniuses" game.

---
+++ src/game/GameLauncher.java
@@ -0,0 +1,10 @@
+package game;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        Game evilGeniusesGame  = new Game();
+
+        // Start the game
+        evilGeniusesGame .startGame();
+    }
+}
